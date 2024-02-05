046a052243a3d055dd57cc74ee5d0ea10296cd09
abdel <holbertonc21@gmail.com>
2024-02-05 16:41:04 +0100

    adjusted the code to render as expected

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,15 +1,22 @@
 $(document).ready(function() {
+  let amenitiesChecked = {};
 
-    let amenitiesChecked = {};
-  
-    $('.amenities input[type="checkbox"]').change(function() {
+  $('.amenities input[type="checkbox"]').change(function() {
       if (this.checked) {
-        amenitiesChecked[$(this).data('id')] = $(this).data('name');
+          amenitiesChecked[$(this).data('id')] = $(this).data('name');
       } else {
-        delete amenitiesChecked[$(this).data('id')];
+          delete amenitiesChecked[$(this).data('id')];
       }
-  
-      $('.amenities h4').text(Object.values(amenitiesChecked).join(', '));
-    });
+
+      // Convertir les valeurs sélectionnées en chaîne et limiter le nombre de caractères
+      let selectedAmenities = Object.values(amenitiesChecked).join(', ');
+      const maxLength = 37; // Définir la limite de caractères ici
+
+      // Vérifier si la longueur dépasse la limite
+      if (selectedAmenities.length > maxLength) {
+          selectedAmenities = selectedAmenities.substring(0, maxLength) + '...';
+      }
+
+      $('.amenities h4').text(selectedAmenities);
   });
-  
+});
