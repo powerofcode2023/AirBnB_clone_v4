ce51876e4f2217424ed0ebc3293daa70b42f3897
abdel <holbertonc21@gmail.com>
2024-02-08 09:49:26 +0100

    fixed code to avoid the delete of h4 tag

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,22 +1,26 @@
 $(document).ready(function() {
-  let amenitiesChecked = {};
+    let amenitiesChecked = {};
 
-  $('.amenities input[type="checkbox"]').change(function() {
-      if (this.checked) {
-          amenitiesChecked[$(this).data('id')] = $(this).data('name');
-      } else {
-          delete amenitiesChecked[$(this).data('id')];
-      }
+    $('.amenities input[type="checkbox"]').change(function() {
+        if (this.checked) {
+            amenitiesChecked[$(this).data('id')] = $(this).data('name');
+        } else {
+            delete amenitiesChecked[$(this).data('id')];
+        }
 
-      // Convertir les valeurs sélectionnées en chaîne et limiter le nombre de caractères
-      let selectedAmenities = Object.values(amenitiesChecked).join(', ');
-      const maxLength = 37; // Définir la limite de caractères ici
+        let selectedAmenities = Object.values(amenitiesChecked).join(', ');
+        const maxLength = 37; // Définir la limite de caractères ici
 
-      // Vérifier si la longueur dépasse la limite
-      if (selectedAmenities.length > maxLength) {
-          selectedAmenities = selectedAmenities.substring(0, maxLength) + '...';
-      }
+        // Vérifier si la longueur dépasse la limite
+        if (selectedAmenities.length > maxLength) {
+            selectedAmenities = selectedAmenities.substring(0, maxLength) + '...';
+        }
 
-      $('.amenities h4').text(selectedAmenities);
-  });
+        // Utiliser un espace non-sécable si aucune commodité n'est sélectionnée
+        if (selectedAmenities.length === 0) {
+            selectedAmenities = "&#160;"; // Espace non-sécable HTML
+        }
+
+        $('.amenities h4').html(selectedAmenities); // Utiliser .html() au lieu de .text() pour permettre l'entité HTML
+    });
 });
